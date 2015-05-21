79b3df2c327f4278d76627663563b54785bf30e4
Amit Kumar <amitmac2@gmail.com>
2015-05-22 02:31:02 +0530

    correcting classification

--- Bayes Classification/gaussianBayesClassifier.m
+++ Bayes Classification/gaussianBayesClassifier.m
@@ -6,7 +6,7 @@ function class = gaussianBayesClassifier(train,test)
     % Segment the data according to target/class and process
     uniqueY = unique(Y);
     test = test(:,1:n-1);
-    class = zeros(size(test));
+    class = zeros(size(test,1),1);
     for i = 1:length(uniqueY)
         d = X(uniqueY(i) == train(:,n));
         mu = mean(d);
@@ -16,9 +16,9 @@ function class = gaussianBayesClassifier(train,test)
             % Suppose likelihood is given by normal distribution
             likelihood(i,:) = normpdf(test(i,:),mu,vars);
         end
-        likelihood = prod(likelihood,2);
+        plikelihood = prod(likelihood,2);
         prior = length(d)/m;
         
-        class = (likelihood*prior) > class;
+        class(plikelihood*prior > class) = uniqueY(i);
     end
 end
